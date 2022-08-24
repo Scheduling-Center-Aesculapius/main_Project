0a0827996e0b4e5a585f9ccf25d313e0a621950a
Lipe-albuquerque <filipe.inte@gmail.com>
2022-08-24 17:52:53 -0300

    instalação e começo da base de usuarios spring

---
+++ Scheduling-Center-Aesculapius/src/main/java/br/com/schedulingcenteraesculapius/SchedulingCenterAesculapiusApplication.java
@@ -0,0 +1,19 @@
+package br.com.schedulingcenteraesculapius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingCenterAesculapiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingCenterAesculapiusApplication.class, args);
+
+
+		System.out.println("Olá mundo");
+	}
+
+	
+
+
+}
---
+++ Scheduling-Center-Aesculapius/src/test/java/br/com/schedulingcenteraesculapius/SchedulingCenterAesculapiusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.schedulingcenteraesculapius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingCenterAesculapiusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
