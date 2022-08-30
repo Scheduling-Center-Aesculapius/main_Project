007c832a8aff66f818c7baef969a3e9db97d0e96
ariana-ssilva <arianamoreira5021@gmail.com>
2022-08-30 17:20:24 -0300

    main-page added

--- main/src/app/app-routing.module.ts
+++ main/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPatientComponent } from './login-patient/login-patient.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/login-patient', component:LoginPatientComponent},
+  {path: '/main-page', component:MainPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- main/src/app/app.module.ts
+++ main/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPatientComponent } from './login-patient/login-patient.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPatientComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
