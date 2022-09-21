bdc1d9793057f49b3be9d2d230c34c67a71a62d0
Lipe-Albuquerque <filipe.intel@gmail.com>
2022-09-21 20:14:45 -0300

    filipe - validação de login

--- main/src/app/components/navbar/navbar.component.ts
+++ main/src/app/components/navbar/navbar.component.ts
@@ -4,36 +4,29 @@ import { SecurityService } from 'src/app/security.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  links:any=new Array();
+  links: any = new Array();
 
-
-  constructor(
-    public securirity:SecurityService
-  ) { }
+  constructor(public securirity: SecurityService) {}
 
   ngOnInit(): void {
-
-    if (this.securirity.logged) {
-    this.links.push({title:"Home",route:"home-patient"});
-    this.links.push({title:"About",route:"home-patient"});
-    this.links.push({title:"Professionals",route:"register-professional"});
-    this.links.push({title:"Specialties",route:"home-patient"});
-    this.links.push({title:"Contact us",route:"home-patient"});
-    this.links.push({title:"Olá Admin, Bem vindo!", route:"---"});
-    }else{
-    this.links.push({title:"Home",route:"home-patient"});
-    this.links.push({title:"About",route:"home-patient"});
-    this.links.push({title:"Professionals",route:"register-professional"});
-    this.links.push({title:"Specialties",route:"home-patient"});
-    this.links.push({title:"Contact us",route:"home-patient"});
-    this.links.push({title:"Login",route:"login-patient"});
-    this.links.push({title:"Register",route:"register-patient"});
-
+    if (!this.securirity.logged) {
+      this.links.push({ title: 'Home', route: 'home-patient' });
+      this.links.push({ title: 'About', route: 'home-patient' });
+      this.links.push({title: 'Professionals',route: '',});
+      this.links.push({ title: 'Specialties', route: 'home-patient' });
+      this.links.push({ title: 'Contact us', route: 'home-patient' });
+      this.links.push({ title: 'Login', route: 'login-patient' });
+      this.links.push({ title: 'Register', route: 'register-patient' });
+    } else {
+      this.links.push({ title: 'Home', route: 'home-patient' });
+      this.links.push({ title: 'About', route: 'home-patient' });
+      this.links.push({ title: 'Professionals', route: '' });
+      this.links.push({ title: 'Specialties', route: 'home-patient' });
+      this.links.push({ title: 'Contact us', route: 'home-patient' });
+      this.links.push({ title: 'Olá, Bem vindo!', route: '---' });
     }
-
   }
-
 }
