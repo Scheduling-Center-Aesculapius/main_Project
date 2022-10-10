8e9d552759975decea6a1886a3eb827f0d319d08
ariana-ssilva <arianamoreira5021@gmail.com>
2022-10-10 17:19:49 -0300

    Ariana: changing register form;

--- main/src/app/components/Patient/register-patient-form/register-patient-form.component.ts
+++ main/src/app/components/Patient/register-patient-form/register-patient-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-patient-form',
@@ -8,72 +8,52 @@ import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms'
 
 })
 export class RegisterPatientFormComponent implements OnInit {
-  personalInformation!: FormGroup;
-  addressInformation!: FormGroup;
-  otherInformations!: FormGroup;
-  personal_step = false;
-  address_step = false;
-  education_step = false;
-  step = 1;
+  form!: FormGroup;
+  submitted = false;
+
+
 
   constructor(private formBuilder: FormBuilder) { }
-  ngOnInit() {
-    this.personalInformation = this.formBuilder.group({
-      name: ['', Validators.required],
-      lastName: ['', Validators.required,],
-      cpf: ['', Validators.required],
-      phone: ['', Validators.required],
-      birthdate: ['', Validators.required],
-      email: ['', Validators.required],
-      sex: [''],
-      motherName: ['', Validators.required],
-      fatherName: ['', Validators.required],
-    });
-    this.addressInformation = this.formBuilder.group({
-      zipcode: ['', Validators.required],
-      city: ['', Validators.required],
-      street: ['', Validators.required],
-      numberAddress: ['', Validators.required],
-      state: ['', Validators.required],
-      country: ['', Validators.required],
 
-    });
-    this.otherInformations = this.formBuilder.group({
-      allergies: [''],
-    });
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+        lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+        cpf: ['', [Validators.required, Validators.pattern("([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})")]],
+        phoneNumber: ['', [Validators.required, Validators.pattern("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")]],
+        email: ['', [Validators.required, Validators.email]],
+        professional: ['', [Validators.requiredTrue]],
+        birthdate: ['', [Validators.pattern("(0?[1-9]|[12]\d|30|31)[^\w\d\r\n:](0?[1-9]|1[0-2])[^\w\d\r\n:](\d{4}|\d{2})")]],
+        motherName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+        fatherName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+        street: ['', [Validators.required]],
+        numberAddress: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        zipcode: ['', [Validators.required]],
+        state: ['', [Validators.required]],
+        country: ['', [Validators.required]],
+        
+        
+      },
+    );
+    console.log(this.form);
   }
 
-  get personal() { return this.personalInformation.controls; }
-  get address() { return this.addressInformation.controls; }
-  get education() { return this.otherInformations.controls; }
-  next() {
-    if (this.step == 1) {
-      this.personal_step = true;
-      if (this.personalInformation.invalid) { return }
-      this.step++
-    }
-    else if (this.step == 2) {
-      this.address_step = true;
-      if (this.addressInformation.invalid) { return }
-      this.step++;
+  hasError(field: string) {
+    return this.form.get(field)?.errors;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.form.value);
+    if (this.form.valid) {
+      console.log('submit')
     }
   }
 
-  previous() {
-    this.step--
-    if (this.step == 1) {
-      this.address_step = false;
-    }
-    if (this.step == 2) {
-      this.education_step = false;
-    }
-  }
-
-  submit() {
-    if (this.step == 3) {
-      this.education_step = true;
-      if (this.otherInformations.invalid) { return }
-      alert("Well done!!")
-    }
+  onCancel() {
+    this.submitted = false;
+    this.form.reset();
   }
 }
