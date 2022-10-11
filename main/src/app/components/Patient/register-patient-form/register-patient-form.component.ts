56fa67a4809746204e146be1420986c095b4e688
ariana-ssilva <arianamoreira5021@gmail.com>
2022-10-10 21:50:19 -0300

    Ariana: * Added validator to match password and confirm password field

--- main/src/app/components/Patient/register-patient-form/register-patient-form.component.ts
+++ main/src/app/components/Patient/register-patient-form/register-patient-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, FormControl, AbstractControl } from '@angular/forms';
 import { PatientsService } from 'src/app/services/patients.service';
+import { ConfirmedValidator } from '../../Shared/confirmed.validator';
 
 @Component({
   selector: 'app-register-patient-form',
@@ -23,7 +24,10 @@ export class RegisterPatientFormComponent implements OnInit {
         lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
         cpf: ['', [Validators.required, Validators.pattern("([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})")]],
         phoneNumber: ['', [Validators.required, Validators.pattern("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")]],
+        sex: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(20)],
+        confirmPassword: ['', Validators.required, Validators.minLength(8), Validators.maxLength(20)],
         professional: ['', [Validators.requiredTrue]],
         birthdate: ['', [Validators.pattern("(0?[1-9]|[12]\d|30|31)[^\w\d\r\n:](0?[1-9]|1[0-2])[^\w\d\r\n:](\d{4}|\d{2})")]],
         motherName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
@@ -34,9 +38,12 @@ export class RegisterPatientFormComponent implements OnInit {
         zipcode: ['', [Validators.required]],
         state: ['', [Validators.required]],
         country: ['', [Validators.required]],
-        
-        
+
+
       },
+      {
+        validator: ConfirmedValidator('password', 'confirmPassword')
+      }
     );
     console.log(this.form);
   }
@@ -45,12 +52,12 @@ export class RegisterPatientFormComponent implements OnInit {
     return this.form.get(field)?.errors;
   }
 
+  get f() {
+    return this.form.controls;
+  }
+
   register() {
     this.submitted = true;
-    // console.log(this.form.value);
-    // if (this.form.valid) {
-    //   console.log(this.form)
-    // }
     this.ps.save(this.form.value).subscribe(result => console.warn(result));
   }
 
